c5dfdea13474a33c5855f8f9d4fcd00641321a22
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-11-20 18:09:50 +0100

    Update the normalizeUrl function to ignore the '/' at the end of an URL

--- crawl.js
+++ crawl.js
@@ -1,6 +1,10 @@
 function normalizeUrl(urlstring) {
   const urlObj = new URL(urlstring);
-  return `${urlObj.host}${urlObj.pathname}`;
+  const hostPath = `${urlObj.host}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = { normalizeUrl };
--- crawl.test.js
+++ crawl.test.js
@@ -1,8 +1,14 @@
 const { normalizeUrl } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeUrl strip protocol", () => {
+test("normalizeUrl ", () => {
   const input = "https://blog.boot.dev/path";
+  const actual = normalizeUrl(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+test("normalizeUrl strip protocol", () => {
+  const input = "https://blog.boot.dev/path/";
   const actual = normalizeUrl(input);
   const expected = "blog.boot.dev/path";
   expect(actual).toEqual(expected);
