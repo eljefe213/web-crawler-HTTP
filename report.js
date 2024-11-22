538fa52a17a985efcd565875f3bd43ea75f856cf
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-11-22 17:29:58 +0100

    Add the sortPages function

--- report.js
+++ report.js
@@ -1,3 +1,11 @@
 function sortPages(pages) {
-  return [];
+  pagesArr = Object.entries(pages);
+  pagesArr.sort((a, b) => {
+    aHits = a[1];
+    bHits = b[1];
+    return b[1] - a[1];
+  });
+  return pagesArr;
 }
+
+module.exports = { sortPages };
